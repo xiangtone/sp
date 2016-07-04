3b1f2b14901022c923894dcab407a9bcc2733a27
Andy.Chen <chenls@bjxiangtone.com>
2016-07-04 10:20:24 +0800

    add model

---
+++ PlatformBase/src/com/system/model/BaseDataShowModel.java
@@ -0,0 +1,25 @@
+package com.system.model;
+
+public class BaseDataShowModel
+{
+	private int title;
+	private float amount;
+	
+	public int getTitle()
+	{
+		return title;
+	}
+	public void setTitle(int title)
+	{
+		this.title = title;
+	}
+	public float getAmount()
+	{
+		return amount;
+	}
+	public void setAmount(float amount)
+	{
+		this.amount = amount;
+	}
+	
+}
