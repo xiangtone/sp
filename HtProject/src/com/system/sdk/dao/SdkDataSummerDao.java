60ad9a40e406a80a750b08bf25d110a5a568f93a
qinyinsmile <692032586@qq.com>
2016-08-08 15:26:06 +0800

    sdk报表增加数据展示列

--- HtProject/src/com/system/sdk/dao/SdkDataSummerDao.java
+++ HtProject/src/com/system/sdk/dao/SdkDataSummerDao.java
@@ -3,7 +3,9 @@ package com.system.sdk.dao;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.system.database.JdbcGameControl;
 import com.system.database.QueryCallBack;
@@ -13,8 +15,9 @@ import com.system.util.StringUtil;
 public class SdkDataSummerDao
 {
 	@SuppressWarnings("unchecked")
-	public List<SdkDataSummerModel> loadSdkDataSummer(int cpId,int channelId,int appId,int troneId,int spTroneId,String startDate,String endDate,int showType)
+	public Map<String, Object> loadSdkDataSummer(int cpId,int channelId,int appId,int troneId,int spTroneId,String startDate,String endDate,int showType)
 	{
+		Map<String, Object> map = new HashMap<String, Object>();
 		String[] joinQuery = getShowType(showType);
 		String queryParams = joinQuery[0];
 		String joinId = joinQuery[1];
@@ -53,6 +56,7 @@ public class SdkDataSummerDao
 		sql += " SELECT " + joinId + " join_id, " + queryParams + " show_title,";
 		sql += " SUM(data_rows) trone_request_rows, ";
 		sql += " SUM(CASE WHEN STATUS <> 99 THEN data_rows END) trone_effect_request_rows, ";
+		sql += " SUM(CASE WHEN STATUS <> 99 THEN data_rows*b.price END)/100 effect_amount, ";
 		sql += " SUM(CASE WHEN STATUS = 3 OR STATUS = 4 OR STATUS = 5 OR STATUS = 0 THEN data_rows END) trone_order_rows, ";
 		sql += " SUM(CASE WHEN STATUS = 4 OR STATUS = 0 THEN data_rows END) msg_rows, ";
 		sql += " SUM(CASE WHEN STATUS = 0 THEN data_rows END) suc_rows, ";
@@ -88,13 +92,23 @@ public class SdkDataSummerDao
 		
 		sql += " GROUP BY join_id ";
 		sql += " )b ON a.join_id = b.join_id ";
-		
-		return (List<SdkDataSummerModel>)new JdbcGameControl().query(sql, new QueryCallBack()
+		final List<Object> countList = new ArrayList<Object>();
+		map.put("list", (List<SdkDataSummerModel>)new JdbcGameControl().query(sql, new QueryCallBack()
 		{
 			@Override
 			public Object onCallBack(ResultSet rs) throws SQLException
 			{
 				List<SdkDataSummerModel> list = new ArrayList<SdkDataSummerModel>();
+				int countActRows=0;
+				int countUserRows=0;
+				int countTroReqRows=0;
+				int countEffReqRows=0;
+				int countTroOrdRows=0;
+				int countMsgRows=0;
+				int countSucRows=0;
+				float countAmount=0;
+				float countEffAmount=0;
+				
 				while(rs.next())
 				{
 					SdkDataSummerModel model = new SdkDataSummerModel();
@@ -108,12 +122,42 @@ public class SdkDataSummerDao
 					model.setMsgRows(rs.getInt("msg_rows"));
 					model.setSucRows(rs.getInt("suc_rows"));
 					model.setAmount(rs.getFloat("amount"));
+					model.setEffectAmount(rs.getFloat("effect_amount"));
+					countActRows+=model.getActivityRows();
+					countUserRows+=model.getUserRows();
+					countTroReqRows+=model.getTroneRequestRows();
+					countEffReqRows+=model.getTroneEffectRequestRows();
+					countTroOrdRows+=model.getTroneOrderRows();
+					countMsgRows+=model.getMsgRows();
+					countSucRows+=model.getSucRows();
+					countAmount+=model.getAmount();
+					countEffAmount+=model.getEffectAmount();
 					
 					list.add(model);
 				}
+				countList.add(countActRows);
+				countList.add(countUserRows);
+				countList.add(countTroReqRows);
+				countList.add(countEffReqRows);
+				countList.add(countTroOrdRows);
+				countList.add(countMsgRows);
+				countList.add(countSucRows);
+				countList.add(countAmount);
+				countList.add(countEffAmount);
 				return list;
 			}
-		});
+		}));
+		map.put("countActRows", countList.get(0));
+		map.put("countUserRows", countList.get(1));
+		map.put("countTroReqRows", countList.get(2));
+		map.put("countEffReqRows", countList.get(3));
+		map.put("countTroOrdRows", countList.get(4));
+		map.put("countMsgRows", countList.get(5));
+		map.put("countSucRows", countList.get(6));
+		map.put("countAmount", countList.get(7));
+		map.put("countEffAmount", countList.get(8));
+
+		return map;
 		
 	}
 	
--- HtProject/src/com/system/sdk/server/SdkDataSummerServer.java
+++ HtProject/src/com/system/sdk/server/SdkDataSummerServer.java
@@ -1,13 +1,9 @@
 package com.system.sdk.server;
-
-import java.util.List;
-
+import java.util.Map;
 import com.system.sdk.dao.SdkDataSummerDao;
-import com.system.sdk.model.SdkDataSummerModel;
-
 public class SdkDataSummerServer
 {
-	public List<SdkDataSummerModel>loadSdkDataSummerModel(int cpId,int channelId,int appId,int troneId,int spTroneId,String startDate,String endDate,int showType){
+	public Map<String, Object>loadSdkDataSummerModel(int cpId,int channelId,int appId,int troneId,int spTroneId,String startDate,String endDate,int showType){
 		return new SdkDataSummerDao().loadSdkDataSummer(cpId, channelId, appId, troneId, spTroneId, startDate, endDate, showType);
 	}
 	
